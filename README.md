a4a4f3e4615622f998cb6690068b1bb6e4000e16
Gergely Lendvay <gege.lendvay2003@gmail.com>
2023-10-09 00:23:35 +0200

    License and documentation

---
+++ README.md
@@ -0,0 +1,22 @@
+# Password Generator
+This program helps you generate strong and random passwords based on your preferences. It offers flexibility in password length and character types while also evaluating the strength of the generated password.
+
+## Features
+* **Custom Password Length:** You can choose the length of the password
+* **Character Selection:** You can choose the desired character types to be included in the generated passwords, like lowercase letters, uppercase letters, digits and symbols
+* **Password Strength Evaluation:** After the password is generated, its strength gets evaluated, and classified as Weak, Medium or Strong
+* **Clipboard Integration:** For convenience, the generated password is automatically copied to the clipboard
+
+## Installation
+1. Clone the repository
+2. Install the requirements using `pip install -r requirements.txt`
+3. Run the program using `python main.py`
+
+## Usage
+1. Run the program and follow the prompts to specify password length and character types
+2. The program will generate a password based on your preferences
+3. The password will be automatically copied to the clipboard
+4. You will receive feedback on the strength of the generated password
+
+## License
+The project is licensed under the MIT License
--- main.py
+++ main.py
@@ -5,11 +5,14 @@ import re
 import string
 
 print("Welcome to the Python Password Generator!")
+
+#Check if user input for the password length is an integer or not
 try:
     length = int(input("How long would you like your password to be? "))
 except ValueError:
     exit("Error: Password length must be a number.")
 
+#Prompt the user to choose from the available charachter types
 prompt = [
     inquirer.Checkbox("options", "Please select the desired charachter types for your password", ["Lowercase", "Uppercase", "Numbers", "Symbols"], default=["Lowercase"], carousel=True)
 ]
@@ -21,6 +24,7 @@ if not options:
 generated = []
 
 for i in range(length):
+    #For each iteration, randomly select a charachter type from the selected options
     current = random.choice(options)
     if "Lowercase" in current:
         generated.append(random.choice(string.ascii_lowercase))
@@ -33,11 +37,26 @@ for i in range(length):
 
 random.shuffle(generated)
 password = "".join(generated)
+
+#Print the generated password, and copy it to the clipboard
 print(f'Generated password: {password}')
 pyperclip.copy(password)
 
-#Password strength
+#Password strength checker function
 def passwordStrength(password):
+    """
+    Determines the strength of the password
+
+    Args:
+        password (str): The password to be checked
+
+    Returns:
+        str: 'Weak' for passwords with less than 9 chars
+
+            'Strong' for passwords that contain a number and/or a capital letter and a symbol
+
+            'Medium' for every other password
+    """
     if len(password) <= 8:
         return "Weak"
     if re.search(r"\d|[A-Z]", password) and re.search(r"[\W_]", password):
